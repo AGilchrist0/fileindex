84871eb4df90f383a462199bf2c38edcd5f4cb99
AGilchrist0 <aarongilchrist@Aarons-MacBook-Pro-2.local>
2016-02-14 09:20:30 +0000

    Basic .rtf support added

--- fileindexer.py
+++ fileindexer.py
@@ -18,52 +18,74 @@ class FileSorter:
             file_list.append(next_line)
         return file_list
 
+    def write_txt(file_list):
+        content = '*** SORTED FILES ***'
+        content += FileSorter.parse_txt(file_list,'')
+        FileSorter.write('fileindex','.txt',content)
+
     def parse_txt(file_list,tabbing):
         content = ''
         for item in file_list:
             if len(item) > 1 and isinstance(item,list):
                 if len(tabbing) > 0:
-                    content += '\n' + tabbing[:-2] + '↳'
+                    content += '\n' + tabbing[:-4] + '↳ '
                 else:
                     content += '\n' + tabbing
                 content += item[0]
-                tabbing += '  '
+                tabbing += '    '
                 in_item = []
                 x = 0
                 for things in item:
                     if x != 0: in_item.append(item[x])
                     x += 1
                 content += FileSorter.parse_txt(in_item,tabbing)
-                tabbing = tabbing[:-2]
+                tabbing = tabbing[:-4]
             else:
                 if len(tabbing) > 0:
-                    content += '\n' + tabbing[:-2] + '↳'
+                    content += '\n' + tabbing[:-4] + '↳ '
                 else:
                     content += '\n' + tabbing
                 if isinstance(item,list): content += item[0]
                 else: content += item
         return content
 
+    def write_rtf(file_list):
+        content = '{\\rtf1\\ansi\\deff0\n***SORTED FILES***'
+        content += FileSorter.parse_rtf(file_list,'')
+        content += '}'
+        FileSorter.write('fileindex','.rtf',content)
+
+    def parse_rtf(file_list,tabbing):
+        content = ''
+        for item in file_list:
+            if len(item) > 1 and isinstance(item,list):
+                if len(tabbing) > 0:
+                    content += '\\line\n' + tabbing[:-4] + '- '
+                else:
+                    content += '\\line\n' + tabbing
+                content += item[0]
+                tabbing += '\\tab'
+                in_item = []
+                x = 0
+                for things in item:
+                    if x != 0: in_item.append(item[x])
+                    x += 1
+                content += FileSorter.parse_rtf(in_item,tabbing)
+                tabbing = tabbing[:-4]
+            else:
+                if len(tabbing) > 0:
+                    content += '\\line\n' + tabbing[:-4] + '- '
+                else:
+                    content += '\\line\n' + tabbing
+                if isinstance(item,list): content += item[0]
+                else: content += item
+        return content
 
     def write(file_name,file_extension,content):
         file = file_name+file_extension
         with codecs.open(file,'w',encoding='utf8') as text_file:
             text_file.write(content)
 
-'''def sort_files(tabbing):
-    list = ''
-    for item in os.listdir():
-        next_line = '\n'+tabbing+(item)
-        print(next_line)
-        list += next_line
-        if os.path.isdir(item):
-            tabbing+='    '
-            os.chdir(os.getcwd()+'/'+item+'/')
-            list += sort_files(tabbing)
-            tabbing = tabbing[:-4]
-            os.chdir('..')
-    return list
-'''
 file_list = FileSorter.sort_files()
-content = FileSorter.parse_txt(file_list,'')
-FileSorter.write('fileindex','.txt',content)
+FileSorter.write_rtf(file_list)
+FileSorter.write_txt(file_list)
